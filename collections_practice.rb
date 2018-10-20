43756ca238d5ce3582bf37c3a4128ca7729adb7b
Andrew Johnson <ajohnson.uva@gmail.com>
2018-10-20 17:51:10 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -33,12 +33,12 @@ def count_elements(array)
   array.each do |hash|
     hash[:count] = 1
   end
-    array.each do |hash|
-      hash.sort do |a, b|
-        if a == b
-          a[:count] += 1
-        end
+  array.each do |hash|
+    hash.sort do |a, b|
+      if a == b
+        a[:count] += 1
       end
     end
+  end
   array.uniq
 end
